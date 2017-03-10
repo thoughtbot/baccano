e7c842099fc017cdc6cda7d738794c049a74fa60
Will H McMahan <will@thoughtbot.com>
2017-03-10 13:22:05 -0500

    Add support for long hair and ponytail hair style

--- avatar.rb
+++ avatar.rb
@@ -1,13 +1,14 @@
 class Avatar
-  attr_reader :body_color, :hair, :hair_color, :background_color, :skin_tone, :glasses
+  attr_reader :body_color, :hair, :hair_color, :background_color, :skin_tone, :glasses, :hair_background
 
-  def initialize(body_color, background_color, skin_tone, hair, hair_color, glasses)
+  def initialize(body_color, background_color, skin_tone, hair, hair_color, glasses, hair_background)
     @body_color = body_color
     @background_color = background_color
     @skin_tone = skin_tone
     @hair = hair
     @hair_color = hair_color
     @glasses = glasses
+    @hair_background = hair_background
   end
 
   def shadow_color
