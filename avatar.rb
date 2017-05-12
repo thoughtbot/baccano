1fd1d30eab9e9227a786a9a620c710c9f8658dbf
Will H McMahan <will@mcmahan.me>
2017-05-12 14:37:30 -0400

    Add initial pass on diverse body types

--- avatar.rb
+++ avatar.rb
@@ -1,8 +1,7 @@
 class Avatar
-  attr_reader :body_color, :hair, :hair_color, :background_color, :skin_tone, :glasses, :hair_background, :eyes
+  attr_reader :body_color, :hair, :hair_color, :background_color, :skin_tone, :glasses, :hair_background, :eyes, :body_size
 
-
-  def initialize(body_color, background_color, skin_tone, hair, hair_color, glasses, hair_background, eyes)
+  def initialize(body_color, background_color, skin_tone, hair, hair_color, glasses, hair_background, eyes, body_size)
     @body_color = body_color
     @background_color = background_color
     @skin_tone = skin_tone
@@ -11,6 +10,7 @@ class Avatar
     @glasses = glasses
     @hair_background = hair_background
     @eyes = eyes
+    @body_size = body_size
   end
 
   def shadow_color
