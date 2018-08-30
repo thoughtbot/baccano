64804d871bba9ae354ff531fd3bbc8ca9eddfe62
Matt Jankowski <mjankowski@thoughtbot.com>
2018-08-30 11:32:35 -0400

    Add coverage for random response changing between requests

--- tests/smoke_test.rb
+++ tests/smoke_test.rb
@@ -13,6 +13,20 @@ class SmokeTest < MiniTest::Test
     assert_equal 200, last_response.status
   end
 
+  def test_random_changes_response
+    Random.stub :new_seed, 123 do
+      get "/random"
+    end
+    first_svg = last_response.body
+
+    Random.stub :new_seed, 456 do
+      get "/random"
+    end
+    second_svg = last_response.body
+
+    assert first_svg != second_svg
+  end
+
   def test_root
     get "/"
     assert_equal 302, last_response.status
